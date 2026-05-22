8c50ed53c15d0bb9a00d2012a03510562745c841
Myron Gourlis <myrgourlis@gmail.com>
2026-05-23 00:44:38 +0300

    feat(tests): add InMemoryMessageSubscriber to expected names in TestingSubmoduleExports

--- tests/test_package_exports.py
+++ tests/test_package_exports.py
@@ -58,6 +58,7 @@ class TestTestingSubmoduleExports:
         "FakeSnapshotStore",
         "FakeUnitOfWork",
         "InMemoryMessageBroker",
+        "InMemoryMessageSubscriber",
         "InMemoryProjectionStore",
         "ProjectionStore",
     ]
